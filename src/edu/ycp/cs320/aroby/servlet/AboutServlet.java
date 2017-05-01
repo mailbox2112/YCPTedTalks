335bc3c092d6f10b500d2aeb61e83edd68ddb8d0
Chihea Locke <clocke3@ycp.edu>
2017-04-30 22:30:27 -0400

    //about servlet

--- src/edu/ycp/cs320/aroby/servlet/AboutServlet.java
+++ src/edu/ycp/cs320/aroby/servlet/AboutServlet.java
@@ -6,13 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import edu.ycp.cs320.aroby.controller.AccountController;
-import edu.ycp.cs320.aroby.controller.LoginController;
-import edu.ycp.cs320.aroby.model.Account;
-import edu.ycp.cs320.aroby.model.Professor;
-import edu.ycp.cs320.aroby.model.Student;
 
 public class AboutServlet extends HttpServlet {
 
