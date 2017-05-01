35e2fef1a0754850a8d9a98489bf65abe3ffd818
terrell <tclark22@ycp.edu>
2017-04-30 20:43:12 -0400

    //

--- src/edu/ycp/cs320/aroby/model/Review.java
+++ src/edu/ycp/cs320/aroby/model/Review.java
@@ -3,7 +3,7 @@ package edu.ycp.cs320.aroby.model;
 import java.time.ZonedDateTime;
 import java.util.Comparator;
 
-public class Review implements Comparator<Review>{
+public class Review {
 	private String review, recommendation;
 	private int rating, accountId, tedTalkId, reviewId;
 	private ZonedDateTime date;
@@ -51,57 +51,4 @@ public class Review implements Comparator<Review>{
 	public void setReviewId(int review_id) {
 		this.reviewId = review_id;
 	}
-	public int compare(Review obj, Review obj1){
-		ZonedDateTime a = ZonedDateTime.parse(obj.getDate());
-		ZonedDateTime b = ZonedDateTime.parse(obj1.getDate());
-		if(a.getYear() > b.getYear()){
-			return 1;
-		}
-		else if(a.getYear() < b.getYear()){
-			return -1;
-		}
-		else if(a.getYear() == b.getYear()){
-			if(a.getDayOfYear() > b.getDayOfYear()){
-				return 1;
-			}
-			else if(a.getDayOfYear() < b.getDayOfYear()){
-				return -1;
-			}
-			else if(a.getDayOfYear() == b.getDayOfYear()){
-				if(a.getHour() > b.getHour()){
-					return 1;
-				}
-				else if(a.getHour() < b.getHour()){
-					return -1;
-				}
-				else if(a.getHour() == b.getHour()){
-					if(a.getMinute() > b.getMinute()){
-						return 1; 
-					}
-					else if(a.getMinute() < b.getMinute()){
-						return -1;
-					}
-					else if(a.getMinute() == b.getMinute()){
-						if(a.getSecond() > b.getSecond()){
-							return 1;
-						}
-						else if(a.getSecond() < b.getSecond()){
-							return -1;
-						}
-						else if(a.getSecond() == b.getSecond()){
-							if(a.getNano() > b.getNano()){
-								return 1;
-							}
-							else if(a.getNano() < b.getNano()){
-								return -1;
-							}
-						}
-					}
-					
-				}
-			}
-		}
-		return 0;
-	}
-
 }
